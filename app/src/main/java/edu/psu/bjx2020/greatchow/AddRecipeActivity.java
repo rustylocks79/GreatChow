b1e78cdd431e557d670b416940987d88829bc238
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-04-25 17:03:48 -0400

    added dynamic lists to addRecipe Scrolling to add and view activities

--- app/src/main/java/edu/psu/bjx2020/greatchow/AddRecipeActivity.java
+++ app/src/main/java/edu/psu/bjx2020/greatchow/AddRecipeActivity.java
@@ -1,13 +1,77 @@
 package edu.psu.bjx2020.greatchow;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddRecipeActivity extends AppCompatActivity {
+    int ingredientCounter;
+    int processCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.add_fab);
+        fab.setOnClickListener(view -> {
+            Snackbar.make(view, "replace with own action", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        });
+
+
+        //****************************************************************************************************//
+
+        //Dynamic Ingredient List
+        ingredientCounter = 1000;
+        findViewById(R.id.enter_ingredient_et_1).setId(ingredientCounter);
+        LinearLayout llIngredientContainer = findViewById(R.id.ll_ingredients_container);
+        Button ingredientAddButton = findViewById(R.id.new_ingredient_button);
+        ingredientAddButton.setOnClickListener(view -> {
+            EditText editText = new EditText(AddRecipeActivity.this);
+            ingredientCounter++;
+            editText.setId(ingredientCounter);
+            editText.setHint(R.string.enter_ingredient);
+            llIngredientContainer.addView(editText);
+
+            //LOG STUFF
+//                View v1 = findViewById(1000);
+//                View vOther = findViewById(ingredientCounter);
+//                Log.d("first", "" + v1.getId());
+//                Log.d("other", "" + vOther.getId());
+        });
+
+
+        //****************************************************************************************************//
+
+        //Dynamic Process List
+        processCounter = 2000;
+        findViewById(R.id.enter_process_et_1).setId(processCounter);
+        LinearLayout llProcessContainer = findViewById(R.id.ll_process_container);
+        Button processAddButton = findViewById(R.id.new_process_button);
+        processAddButton.setOnClickListener(v -> {
+            EditText editText = new EditText(AddRecipeActivity.this);
+            processCounter++;
+            editText.setId(processCounter);
+            editText.setHint(R.string.enter_process_step);
+            llProcessContainer.addView(editText);
+
+            //LOG STUFF
+//                View v1 = findViewById(2000);
+//                View vOther = findViewById(processCounter);
+//                Log.d("first", "" + v1.getId());
+//                Log.d("other", "" + vOther.getId());
+        });
     }
+
+
+
 }
