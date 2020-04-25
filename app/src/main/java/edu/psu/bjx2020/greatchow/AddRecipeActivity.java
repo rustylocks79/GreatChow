acb99b69b1cac0039f4ebf45bd41f092cdccafcb
bdwarner99 <54958683+bdwarner99@users.noreply.github.com>
2020-04-25 14:19:22 -0400

    Added view_recipe and add add_recipe layouts/content

---
+++ app/src/main/java/edu/psu/bjx2020/greatchow/AddRecipeActivity.java
@@ -0,0 +1,13 @@
+package edu.psu.bjx2020.greatchow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
