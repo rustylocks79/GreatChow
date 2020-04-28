8a505dd5d76403db0cea70918ffd95202c8b3309
TheaPxy <theapxy@gmail.com>
2020-04-28 14:56:40 -0400

    made a change.

---
+++ app/src/main/java/edu/psu/bjx2020/greatchow/SelectRecipeActivity.java
@@ -0,0 +1,75 @@
+package edu.psu.bjx2020.greatchow;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import edu.psu.bjx2020.greatchow.db.FirestoreGC;
+import edu.psu.bjx2020.greatchow.db.Recipe;
+
+public class SelectRecipeActivity extends AppCompatActivity {
+    private static final String TAG = "SelectRecipeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_recipe);
+
+        //toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //finish floating button
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        postAuth();
+
+    }
+
+    public void postAuth() {
+        FirestoreGC firestoreGC = FirestoreGC.getInstance();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        LinearLayout selectRecipeList = findViewById(R.id.search_recipe_list_ll);
+        firestoreGC.getAllRecipes(false, false, task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Recipe recipe = document.toObject(Recipe.class);
+                    Button button = new Button(SelectRecipeActivity.this);
+                    button.setText(recipe.getName());
+                    button.setOnClickListener(v -> {
+                        // show add confirm box if need
+                        ///AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+
+                        // add the recipe to calendar
+                    });
+                    selectRecipeList.addView(button);
+                    Log.d(TAG, document.getId() + " => " + recipe.toString());
+                }
+            } else {
+                Log.e(TAG, "Error getting documents: ", task.getException());
+            }
+        });
+    }
+
+    public void confirmErase(View view) {
+        //DialogFragment dialogFragment = new
+    }
+
+}
